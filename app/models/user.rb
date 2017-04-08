0e3121db42b2fe8caa5e1d4c61ca278e0baca427
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-04-07 21:21:42 -0700

    Add seed data when a new user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,31 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :login, presence: true, uniqueness: true
+
+  after_commit :add_seed_data, on: :create
+
+  private
+
+  def add_seed_data
+    seed_file = File.read(Rails.root.join('db', 'seeds', 'flashcards.yml'))
+    seed_data = YAML.safe_load(seed_file)
+
+    flashcards = seed_data.map do |flashcard|
+      tags = flashcard['tags'].map do |tag|
+        Tag.find_or_create_by!(
+          user: self,
+          name: tag
+        )
+      end
+
+      Flashcard.new(
+        user: self,
+        question: flashcard['question'],
+        answer: flashcard['answer'],
+        tags: tags
+      )
+    end
+
+    self.flashcards.push(flashcards)
+  end
 end
